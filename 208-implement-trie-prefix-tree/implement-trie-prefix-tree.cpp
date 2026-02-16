538fce32b7bf6c333c714f6184a0a26b8cb15c7c
Dhairya Doshi <slayerdemon7386@gmail.com>
2026-02-16 11:53:04 +0530

    Time: 101 ms (5.01%) | Memory: 39.2 MB (98.13%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,32 @@
+class Trie {
+public:
+    set<string> cmp, pref; 
+    Trie() {
+        
+    }
+    
+    void insert(string word) {
+        cmp.insert(word);
+        string res;
+        for(auto &x: word){
+            res.push_back(x);
+            pref.insert(res);
+        }
+    }
+    
+    bool search(string word) {
+        return cmp.find(word)!=cmp.end();
+    }
+    
+    bool startsWith(string prefix) {
+        return pref.find(prefix)!=pref.end();
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
