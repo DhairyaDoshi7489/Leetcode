2d84b0beb5a2166b95d010bd5fb38f232940946c
Dhairya Doshi <slayerdemon7386@gmail.com>
2026-03-22 19:46:36 +0530

    Time: 20 ms (23.60%) | Memory: 90.4 MB (96.48%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
         int ct = 0, n = grid.size(), m = grid[0].size();
-        vector<vector<int>> res(n, vector<int> (m));
+        // vector<vector<int>> res(n, vector<int> (m));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                res[i][j]=grid[i][j]+(i?res[i-1][j]:0)+(j?res[i][j-1]:0)-((i&&j)?res[i-1][j-1]:0);
+                grid[i][j]+=(i?grid[i-1][j]:0)+(j?grid[i][j-1]:0)-((i&&j)?grid[i-1][j-1]:0);
                 // cout<<res[i][j]<<" ";
-                if(res[i][j]<=k)ct++;
+                if(grid[i][j]<=k)ct++;
             }
             // cout<<endl;
         }
