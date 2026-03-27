61e8ad4be706b525899508a0ed1e418ce0953bcd
Dhairya Doshi <slayerdemon7386@gmail.com>
2026-03-27 18:29:01 +0530

    Time: 25 ms (82.03%) | Memory: 132.1 MB (65.18%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -5,7 +5,7 @@ public:
         long long val = 1;
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> pre(n, vector<int>(m, 1));
-        vector<vector<int>> post(n, vector<int>(m, 1));
+        // vector<vector<int>> post(n, vector<int>(m, 1));
         int mod = 12345;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
@@ -16,15 +16,15 @@ public:
         val = 1;
         for(int i=n-1;i>-1;i--){
             for(int j=m-1;j>-1;j--){
-                post[i][j] = val;
+                pre[i][j] = (val*pre[i][j])%mod;
                 val*=grid[i][j], val%=mod;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                grid[i][j]=(pre[i][j]*1LL*post[i][j])%mod;
-            }
-        }
-        return grid;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         grid[i][j]=(pre[i][j]*1LL*post[i][j])%mod;
+        //     }
+        // }
+        return pre;
     }
 };
